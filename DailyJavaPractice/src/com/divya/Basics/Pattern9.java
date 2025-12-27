e6106cae573be771bd8902af89820eb609f93ac0
DIVYA K <divyakdiv10@gmail.com>
2025-12-28 00:24:23 +0530

    9 and 10 patterns

---
+++ DailyJavaPractice/src/com/divya/Basics/Pattern9.java
@@ -0,0 +1,21 @@
+package com.divya.Basics;
+
+public class Pattern9 {
+
+	public static void main(String[] args) {
+		for(int j=1; j<=5; j++)
+		{
+			for(int k=1; k<=j; k++)
+			{
+				System.out.print(" ");
+			}
+			for(int i=9; i>=2*j-1 ;i--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
